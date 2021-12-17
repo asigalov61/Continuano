431f4d8c3222490dbd66396721d69610c8ed6b87
Alex <asigalov61@hotmail.com>
2021-12-16 16:29:45 -0800

    Version 1.0 Solo Piano Colab Update/Improvements
    This is for Continuano 1024 embed Solo Piano model

--- Continuano.py
+++ Continuano.py
@@ -40,13 +40,18 @@ get_ipython().system('nvidia-smi')
 get_ipython().system('git clone https://github.com/asigalov61/tegridy-tools')
 get_ipython().system('pip install torch')
 get_ipython().system('pip install tqdm')
+get_ipython().system('pip install matplotlib')
+
+get_ipython().system('apt install fluidsynth #Pip does not work for some reason. Only apt works')
+get_ipython().system('pip install midi2audio')
+get_ipython().system('pip install pretty_midi')
 
 
 # In[ ]:
 
 
 #@title Import all needed modules
-os.chdir('../')
+
 print('Loading needed modules. Please wait...')
 import os
 from datetime import datetime
@@ -55,51 +60,113 @@ import copy
 import tqdm as tqdm
 from tqdm import tqdm
 
+if not os.path.exists('/content/Dataset'):
+    os.makedirs('/content/Dataset')
+
 print('Loading TMIDIX module...')
-os.chdir('tegridy-tools/tegridy-tools')
+os.chdir('/content/tegridy-tools/tegridy-tools')
 import TMIDIX
+
+os.chdir('/content/tegridy-tools/tegridy-tools')
 from GPT2RGAX import *
 
-os.chdir('../')
+import matplotlib.pyplot as plt
+from midi2audio import FluidSynth
+import pretty_midi
+import librosa.display
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+from IPython.display import display, Javascript, HTML, Audio
+
+from google.colab import output, drive
+os.chdir('/content/')
 
 
-# # (Load Continuano Model)
+# # (MODEL)
+
+# In[ ]:
+
+
+#@title  Download Piano Continuano Model
+
+#@markdown Solo Piano
+
+get_ipython().run_line_magic('cd', '/content/')
+
+print('=' * 70)
+print('Downloading pre-trained dataset-model...Please wait...')
+print('=' * 70)
+
+get_ipython().system('wget https://github.com/asigalov61/Continuano/raw/main/Model/1024x1024-Piano-TMD/Continuano-Trained-Model.zip.001')
+get_ipython().system('wget https://github.com/asigalov61/Continuano/raw/main/Model/1024x1024-Piano-TMD/Continuano-Trained-Model.zip.002')
+get_ipython().system('wget https://github.com/asigalov61/Continuano/raw/main/Model/1024x1024-Piano-TMD/Continuano-Trained-Model.zip.003')
+get_ipython().system('wget https://github.com/asigalov61/Continuano/raw/main/Model/1024x1024-Piano-TMD/Continuano-Trained-Model.zip.004')
+get_ipython().system('wget https://github.com/asigalov61/Continuano/raw/main/Model/1024x1024-Piano-TMD/Continuano-Trained-Model.zip.005')
+get_ipython().system('wget https://github.com/asigalov61/Continuano/raw/main/Model/1024x1024-Piano-TMD/Continuano-Trained-Model.zip.006')
+
+get_ipython().system('cat Continuano-Trained-Model.zip* > Continuano-Trained-Model.zip')
+print('=' * 70)
+
+get_ipython().system('unzip -j Continuano-Trained-Model.zip')
+print('=' * 70)
+
+print('Done! Enjoy! :)')
+print('=' * 70)
+get_ipython().run_line_magic('cd', '/content/')
+
+
+# # (LOAD)
 
 # In[ ]:
 
 
 #@title Load/Reload the model
-full_path_to_model_checkpoint = "../Continuano-Trained-Model.pth" #@param {type:"string"}
+full_path_to_model_checkpoint = "/content/Continuano-Trained-Model.pth" #@param {type:"string"}
 
 print('Loading the model...')
 config = GPTConfig(VOCAB_SIZE, 
                    max_seq,
-                   dim_feedforward=dim_feedforward,
+                   dim_feedforward=1024,
                    n_layer=6, 
                    n_head=8, 
-                   n_embd=512,
+                   n_embd=1024,
                    enable_rpr=True,
                    er_len=max_seq)
 
 model = GPT(config).to(get_device())
 
 model.load_state_dict(torch.load(full_path_to_model_checkpoint))
+
+model.eval()
 print('Done!')
 
 
-# # (Load Seed MIDI)
+# # (GENERATE MUSIC)
+
+# ## Custom MIDI option
 
 # In[ ]:
 
 
-data = TMIDIX.Optimus_MIDI_TXT_Processor('../Continuano-Seed.mid', 
+#@title Load your custom MIDI here
+full_path_tp_custom_MIDI = "/content/tegridy-tools/tegridy-tools/seed2.mid" #@param {type:"string"}
+print('=' * 70)
+
+print('Loading custom MIDI...')
+
+print('File name:', full_path_tp_custom_MIDI)
+
+data = TMIDIX.Optimus_MIDI_TXT_Processor(full_path_tp_custom_MIDI, 
                                          dataset_MIDI_events_time_denominator=10, 
                                          perfect_timings=True, 
                                          musenet_encoding=True, 
                                          char_offset=0, 
-                                         MIDI_channel=-1, 
+                                         MIDI_channel=16, 
                                          MIDI_patch=range(0, 127)
                                         )
+print('=' * 70)
+print('Converting to INTs...')
 
 SONG = data[5]
 inputs = []
@@ -110,94 +177,143 @@ for i in SONG:
       
         inputs.extend([256+i[3]])
 
+print('=' * 70)
+print('Done!')
+print('Enjoy! :)')
+print('=' * 70)
 
-# # (GENERATE CONTINUATION)
+
+# ## Generate
 
 # In[ ]:
 
 
 #@title Generate and download a MIDI file
 
-number_of_tokens_to_generate = 1024 #@param {type:"slider", min:8, max:1024, step:8}
-use_random_primer = False #@param {type:"boolean"}
-start_with_zero_token = True #@param {type:"boolean"}
+#@markdown NOTE: The first continuation sample may not be perfect, so generate several samples if you are not getting good results
+
+number_of_tokens_to_generate = 1024 #@param {type:"slider", min:512, max:1024, step:8}
+priming_type = "Custom MIDI" #@param ["Intro", "Outro", "Custom MIDI"]
+custom_MIDI_trim_type = "From Start" #@param ["From Start", "From End"]
+
+temperature = 0.8 #@param {type:"slider", min:0.1, max:1.3, step:0.1}
+
+tokens_range = 3072 #@param {type:"slider", min:512, max:3328, step:256}
+show_stats = True #@param {type:"boolean"}
 
 
-fname = 'Continuano-Composition'
+fname = '/content/Continuano-Music-Composition'
 
-print('Continuano Model Generator')
+print('Continuano Music Model Generator')
 
 output_signature = 'Continuano'
 song_name = 'RGA Composition'
 
-model.eval()
+if show_stats:
+  print('=' * 70)
+  print('Priming type:', priming_type)
+  print('Custom MIDI trim type:', custom_MIDI_trim_type)
+  print('Temperature:', temperature)
+  print('Tokens range:', tokens_range)
 
-if use_random_primer:
-  sequence = [random.randint(10, 387) for i in range(64)]
-  idx = secrets.randbelow(len(sequence))
-  rand_seq = model.generate(torch.Tensor(sequence[idx:idx+120]), target_seq_length=number_of_tokens_to_generate)
-  out = rand_seq[0].cpu().numpy().tolist()
-
-else:
-  out = []
-  
-  try:
-    if start_with_zero_token:
-      sequence = inputs[-512:]
-      rand_seq = model.generate(torch.Tensor(sequence), target_seq_length=number_of_tokens_to_generate, stop_token=256+512)
-      out = rand_seq[0].cpu().numpy().tolist()
-    else:
-      idx = secrets.randbelow(len(train_data))
-      sequence = train_data[idx:idx+512]
-      rand_seq = model.generate(torch.Tensor(sequence), target_seq_length=number_of_tokens_to_generate, stop_token=256+512)
-      out = rand_seq[0].cpu().numpy().tolist()
-  
-  except:
-    print('=' * 50)
-    print('Error! Try random priming instead!')
-    print('Shutting down...')
-    print('=' * 50)
-
-if len(out) != 0:
-  song = []
-  song = out
-  song_f = []
-  time = 0
-
-  pitch = 0
-  duration = 0
-  once = True
-  for s in song:
-    if s >= 0 and s <= 256:
-        time += s
-
-    if s >= 256 and s < 512:
-        pitch = s-256
-        song_f.append(['note', (abs(time))*10, 500, 0, pitch, pitch ])
-  
-    if song.index(s) >= len(sequence) and once:
-        song_f.append(['text_event', abs(time) * 10, 'Continuation Starts Here'])
-        once = False
+print('=' * 70)
+if priming_type == 'Intro':
+    rand_seq = model.generate(torch.Tensor([256+(256 * 11)-1, 
+                                            256+(256 * 11)-3]), 
+                                            target_seq_length=number_of_tokens_to_generate,
+                                            temperature=temperature,
+                                            stop_token=tokens_range,
+                                            verbose=show_stats)
     
-  detailed_stats = TMIDIX.Tegridy_SONG_to_MIDI_Converter(song_f,
-                                                        output_signature = 'Continuano',  
-                                                        output_file_name = '../Continuano-Composition', 
-                                                        track_name='Project Los Angeles', 
-                                                        number_of_ticks_per_quarter=500)
+    out = rand_seq[0].cpu().numpy().tolist()
 
-  print('Done!')
+if priming_type == 'Outro':
+    rand_seq = model.generate(torch.Tensor([256+(256 * 11)-2]), 
+                              target_seq_length=number_of_tokens_to_generate,
+                              temperature=temperature,
+                              stop_token=tokens_range,
+                              verbose=show_stats)
+    
+    out = rand_seq[0].cpu().numpy().tolist()
 
-  print('=' * 70)
-  print('Detailed MIDI stats:')
-  for key, value in detailed_stats.items():
-        print('=' * 70)
-        print(key, '|', value)
+if priming_type == 'Custom MIDI' and inputs != []:
+    out = []
 
-  print('=' * 70)
+    if custom_MIDI_trim_type == 'From Start':
+      sequence = inputs[:512]
+    else:
+      sequence = inputs[-512:]
+
+    rand_seq = model.generate(torch.Tensor(sequence), 
+                              target_seq_length=number_of_tokens_to_generate, 
+                              temperature=temperature,
+                              stop_token=tokens_range,
+                              verbose=show_stats)
+    
+    out = rand_seq[0].cpu().numpy().tolist()
+
+print('=' * 70)
+if len(out) != 0:
+    song = []
+    song = out
+    song_f = []
+    time = 0
+    pitch = 0
+    duration = 0
+    once = True
+    for s in song:
+      if s >= 0 and s <= 256:
+          time += s
+      
+      if s >= 256 and s < 256+256:
+          pitch = s - 256
+          song_f.append(['note', (abs(time))*10, 500, 0, pitch, pitch ])
+    
+      if song.index(s) >= len(sequence) and once:
+          song_f.append(['text_event', abs(time) * 10, 'Continuation Start Here'])
+          once = False
+      
+    detailed_stats = TMIDIX.Tegridy_SONG_to_MIDI_Converter(song_f,
+                                                          output_signature = 'Continuano',  
+                                                          output_file_name = '/content/Continuano-Music-Composition', 
+                                                          track_name='Project Los Angeles', 
+                                                          number_of_ticks_per_quarter=500)
+
+    print('Done!')
+
+    print('Downloading your composition now...')
+    from google.colab import files
+    files.download(fname + '.mid')
+
+    if show_stats:
+      print('=' * 70)
+      print('Detailed MIDI stats:')
+      for key, value in detailed_stats.items():
+            print('=' * 70)
+            print(key, '|', value)
+
+    print('=' * 70)
 
 else:
   print('Models output is empty! Check the code...')
   print('Shutting down...')
+
+print('=' * 70)
+print('Plotting the composition. Please wait...')
+
+fname = '/content/Continuano-Music-Composition'
+
+pm = pretty_midi.PrettyMIDI(fname + '.mid')
+
+# Retrieve piano roll of the MIDI file
+piano_roll = pm.get_piano_roll()
+
+plt.figure(figsize=(14, 5))
+librosa.display.specshow(piano_roll, x_axis='time', y_axis='cqt_note', fmin=1, hop_length=160, sr=16000, cmap=plt.cm.hot)
+plt.title(fname)
+
+FluidSynth("/usr/share/sounds/sf2/FluidR3_GM.sf2", 16000).midi_to_audio(str(fname + '.mid'), str(fname + '.wav'))
+Audio(str(fname + '.wav'), rate=16000)
 
 
 # # Congrats! You did it! :)
